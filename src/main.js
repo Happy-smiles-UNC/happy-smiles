6e4fbd13dadadbd2672215fb7a9d60b8a893d36c
Shweta Deshmukh <drshwetadeshmukh23@gmail.com>
2025-04-26 17:54:52 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,85 +1,58 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import HighchartsVue from 'highcharts-vue'
-import Highcharts from 'highcharts'
-import $ from 'jquery'
-import 'leaflet/dist/leaflet.css'
-
 // Font Awesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-// Import all FontAwesome solid icons you need
+// Correctly combined icons in one import:
 import { 
-  faTooth, 
-  faTeethOpen, 
-  faCheck, 
-  faFire, 
-  faClockRotateLeft, 
+  faTooth,
+  faTeethOpen,
+  faCheck,
+  faFire,
+  faClockRotateLeft,
   faLightbulb,
   faHome,
   faMap,
   faInfoCircle,
-  faToothbrush, 
-  faPumpSoap, 
-  faTriangleExclamation, 
-  faRecycle, 
-  faBolt, 
-  faGlassWaterDroplet, 
-  faAppleWhole, 
-  faSmokingBan, 
-  faChild, 
-  faQuestionCircle, 
+  faToothbrush,
+  faPumpSoap,
+  faTriangleExclamation,
+  faRecycle,
+  faBolt,
+  faGlassWaterDroplet,
+  faAppleWhole,
+  faSmokingBan,
+  faChild,
+  faQuestionCircle,
   faFaceGrinBeam,
   faFaceSmile,
   faUsers
 } from '@fortawesome/free-solid-svg-icons'
 
-// Add all icons to the library
+// Add all icons to library
 library.add(
-  faTooth, 
-  faTeethOpen, 
-  faCheck, 
-  faFire, 
-  faClockRotateLeft, 
+  faTooth,
+  faTeethOpen,
+  faCheck,
+  faFire,
+  faClockRotateLeft,
   faLightbulb,
   faHome,
   faMap,
   faInfoCircle,
-  faToothbrush, 
-  faPumpSoap, 
-  faTriangleExclamation, 
-  faRecycle, 
-  faBolt, 
-  faGlassWaterDroplet, 
-  faAppleWhole, 
-  faSmokingBan, 
-  faChild, 
+  faToothbrush,
+  faPumpSoap,
+  faTriangleExclamation,
+  faRecycle,
+  faBolt,
+  faGlassWaterDroplet,
+  faAppleWhole,
+  faSmokingBan,
+  faChild,
   faQuestionCircle,
   faFaceGrinBeam,
   faFaceSmile,
   faUsers
 )
 
-// Make jQuery available globally
-window.$ = window.jQuery = $
-
-// Create and configure the Vue application
-const app = createApp(App)
-
-// Use router
-app.use(router)
-
-// Use Highcharts
-app.use(HighchartsVue, {
-  highcharts: Highcharts
-})
-
-// Register Font Awesome component globally
+// Register component globally
 app.component('font-awesome-icon', FontAwesomeIcon)
-
-// Mount the app
-app.mount('#app')
