3cfb2affcb8a63e7b2617e61bbcaabec579224a2
Miss Taco <misstaco@msn.com>
2025-04-26 21:31:11 -0400

    fix: white screen

--- src/main.js
+++ src/main.js
@@ -1,58 +1,57 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import HighchartsVue from 'highcharts-vue'
+import Highcharts from 'highcharts'
+import $ from 'jquery'
+import 'leaflet/dist/leaflet.css'
+
 // Font Awesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-// Correctly combined icons in one import:
 import { 
-  faTooth,
-  faTeethOpen,
-  faCheck,
-  faFire,
-  faClockRotateLeft,
+  faTooth, 
+  faTeethOpen, 
+  faCheck, 
+  faFire, 
+  faClockRotateLeft, 
   faLightbulb,
   faHome,
   faMap,
-  faInfoCircle,
-  faToothbrush,
-  faPumpSoap,
-  faTriangleExclamation,
-  faRecycle,
-  faBolt,
-  faGlassWaterDroplet,
-  faAppleWhole,
-  faSmokingBan,
-  faChild,
-  faQuestionCircle,
-  faFaceGrinBeam,
-  faFaceSmile,
-  faUsers
+  faInfoCircle
 } from '@fortawesome/free-solid-svg-icons'
 
-// Add all icons to library
+// Add icons to the library
 library.add(
-  faTooth,
-  faTeethOpen,
-  faCheck,
-  faFire,
-  faClockRotateLeft,
+  faTooth, 
+  faTeethOpen, 
+  faCheck, 
+  faFire, 
+  faClockRotateLeft, 
   faLightbulb,
   faHome,
   faMap,
-  faInfoCircle,
-  faToothbrush,
-  faPumpSoap,
-  faTriangleExclamation,
-  faRecycle,
-  faBolt,
-  faGlassWaterDroplet,
-  faAppleWhole,
-  faSmokingBan,
-  faChild,
-  faQuestionCircle,
-  faFaceGrinBeam,
-  faFaceSmile,
-  faUsers
+  faInfoCircle
 )
 
-// Register component globally
+// Make jQuery available globally
+window.$ = window.jQuery = $
+
+// Create and configure the Vue application
+const app = createApp(App)
+
+// Use router
+app.use(router)
+
+// Use Highcharts
+app.use(HighchartsVue, {
+  highcharts: Highcharts
+})
+
+// Register Font Awesome component globally
 app.component('font-awesome-icon', FontAwesomeIcon)
+
+// Mount the app
+app.mount('#app')
